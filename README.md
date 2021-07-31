f5e9c14f791e11f262a6e3769f8f5bad347dec5c
iankorf <ifkorf@ucdavis.edu>
2021-07-30 23:58:14 -0700

    edits

--- README.md
+++ README.md
@@ -2,10 +2,11 @@
 
 ## MANIFEST ##
 
-+ asg.py - alternative splice generator
 + isoform.py - library for building all python-based projects here
++ asg.py - alternative splice generator
 + randomly.py - examines splicing in random sequences
-+ txamatic - C-based transcript generator
-
-
++ txamatic - C-based transcript generators
++ data - directory training data and scripts
+	+ extract_sequences.py - uses Lyman2020 favorites
+	+ build_models.py - creates pwms, etc
 
--- data/build_models.py
+++ data/build_models.py
@@ -11,34 +11,34 @@ def get_seqs(filename):
 			seqs.append(seq.rstrip())
 	return seqs
 
-exons = get_seqs('data/exon.txt.gz')
-introns = get_seqs('data/intron.txt.gz')
-accs = get_seqs('data/acceptor.txt.gz')
-dons = get_seqs('data/donor.txt.gz')
+exons = get_seqs('exon.txt.gz')
+introns = get_seqs('intron.txt.gz')
+accs = get_seqs('acceptor.txt.gz')
+dons = get_seqs('donor.txt.gz')
 
 # Exon Lengths
 elen = isoform.create_len(exons, 25, 1000)
-isoform.write_len('data/exon.len', elen)
+isoform.write_len('exon.len', elen)
 
 # Intron Lengths
 ilen = isoform.create_len(introns, 5, 1000)
-isoform.write_len('data/intron.len', ilen)
+isoform.write_len('intron.len', ilen)
 
 # Acceptor PWM
 apwm = isoform.create_pwm(accs)
-isoform.write_pwm('data/acceptor.pwm', apwm)
+isoform.write_pwm('acceptor.pwm', apwm)
 
 # Donor PWM
 dpwm = isoform.create_pwm(dons)
-isoform.write_pwm('data/donor.pwm', dpwm)
+isoform.write_pwm('donor.pwm', dpwm)
 
 # Exon Markov model
 emm = isoform.create_markov(exons, 3, 0, 0)
-isoform.write_markov('data/exon.mm', emm)
+isoform.write_markov('exon.mm', emm)
 
 # Intron Markov model
 imm = isoform.create_markov(introns, 3, 5, 6)
-isoform.write_markov('data/intron.mm', imm)
+isoform.write_markov('intron.mm', imm)
 
 
 
