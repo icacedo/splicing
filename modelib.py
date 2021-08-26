5faf18ad4a21cdb8869479c89db4ff4d7cdf7498
ismael <icacedo@ucdavis.edu>
2021-08-26 15:33:32 -0700

    cleaned up code

--- modelib.py
+++ modelib.py
@@ -1,119 +1,81 @@
-import seqlib 
 import random
-import isoform
+import seqlib as sl
 import numpy as np
 
 # n is the length of an individual sequence
 # N is the number of sequences
-def simple_seqgen(n,N):
+def simple_seqgen(n, N):
 
-	seq_list=[]
-	for i in range(1,N+1):
+	seq_list = []
+	for i in range(1, N + 1):
 		#id='>s'+str(i)
-		seq=seqlib.random_dna(n,0.20,0.30,0.30,0.20)
+		seq=sl.random_dna(n,0.20,0.30,0.30,0.20)
 		seq_list.append(seq)
 	return(seq_list)
 
-# first version of pwm code, actually only gets nucleotide frequencies
-# dictionary entries are not in alphabetical order
-def make_simple_pwm(seqs,n,N):
+def make_pfm(seqs, n, N):
 
-	pwm=[{} for i in range(n)]
+	pfm = [
+		{'A':0, 'C':0, 'G':0, 'T':0} for i in range(n)
+		]
 	for seq in seqs:
-		position=0
+		position = 0
 		for nt in seq:
-			if nt not in pwm[position]:
-				pwm[position][nt]=1
-			else:
-				pwm[position][nt]+=1
-			position+=1
-	for i in range(len(pwm)):
-		for nt in pwm[i]:
-			pwm[i][nt]=pwm[i][nt]/N
-	return(pwm)
+			pfm[position][nt] += 1
+			position += 1
+	for i in range(len(pfm)):
+		for nt in pfm[i]:
+			pfm[i][nt] = pfm[i][nt]/N
+	return(pfm)
 
-n=8
-N=4
-seqs=simple_seqgen(n,N)
-pfm=[{'A':0,'C':0,'G':0,'T':0} for i in range(n)]
-position=0
-for seq in seqs:
-	for nt in seq:
-		if nt.upper()=='A':
-			pfm[position][nt]+=1
-	position+=1
-print(pfm)
+def make_pwm(seqs, n, N, pseudo_count):
 
-
-def make_pwm(seqs,n,N):
-
-	pfm_arr=np.zeros(shape=(4,n))
-	A=0
-	C=0
-	G=0
-	T=0
+	pfm_arr = np.zeros(shape=(4, n))
+	A = 0
+	C = 0
+	G = 0
+	T = 0
 	for seq in seqs:
-		position=0
+		position = 0
 		for nt in seq:
-			if nt.upper()=='A':
-				pfm_arr[0][position]+=1
-				A+=1
-			if nt.upper()=='C':
-				pfm_arr[1][position]+=1
-				C+=1
-			if nt.upper()=='G':
-				pfm_arr[2][position]+=1
-				G+=1
-			if nt.upper()=='T':
-				pfm_arr[3][position]+=1
-				T+=1
-			#else: 
-			#	print('unknown character')
-			#	sys.exit()
-			position+=1
-	pfm_arr=np.divide(pfm_arr,N)
-	#print(pfm_arr)
+			if nt.upper() == 'A':
+				pfm_arr[0][position] += 1
+				A += 1
+			if nt.upper() == 'C':
+				pfm_arr[1][position] += 1
+				C += 1
+			if nt.upper() == 'G':
+				pfm_arr[2][position] += 1
+				G += 1
+			if nt.upper() == 'T':
+				pfm_arr[3][position] += 1
+				T += 1
+			position += 1
+	pfm_arr = np.divide(pfm_arr, N) + pseudo_count
 
-	background=A+C+G+T
-	Af=A/background
-	Cf=C/background
-	Gf=G/background
-	Tf=T/background
+	background = A + C + G + T
+	Af = A / background
+	Cf = C / background
+	Gf = G / background
+	Tf = T / background
 
-	pwm_arr=np.zeros(shape=(4,n))
-	pseudo=0.001
-	r=0
+	pwm_arr = np.zeros(shape=(4, n))
+	row = 0
 	for x in pfm_arr:
-		c=0
+		col = 0
 		for y in x:
-			if r==0:
-			# pseudocounting
-				if pfm_arr[r][c]==0:
-					pfm_arr[r][c]+=0.001
-				pwm_arr[r][c]=np.log2(pfm_arr[r][c]/Af)
-			if r==1:
-				if pfm_arr[r][c]==0:
-					pfm_arr[r][c]+=0.001
-				pwm_arr[r][c]=np.log2(pfm_arr[r][c]/Cf)
-			if r==2:
-				if pfm_arr[r][c]==0:
-					pfm_arr[r][c]+=0.001
-				pwm_arr[r][c]=np.log2(pfm_arr[r][c]/Gf)
-			if r==3:
-				if pfm_arr[r][c]==0:
-					pfm_arr[r][c]+=0.001
-				pwm_arr[r][c]=np.log2(pfm_arr[r][c]/Tf)
-			c+=1
-		r+=1
+			if row == 0: 
+				pwm_arr[row][col] = np.log2(pfm_arr[row][col] / Af)
+			if row == 1:
+				pwm_arr[row][col] = np.log2(pfm_arr[row][col] / Cf)
+			if row == 2:
+				pwm_arr[row][col] = np.log2(pfm_arr[row][col] / Gf)
+			if row == 3:
+				pwm_arr[row][col] = np.log2(pfm_arr[row][col] / Tf)
+			col += 1
+		row += 1
 	return(pwm_arr)
-
-# if modelib is imported with active code, it will run
-'''
-n=100
-N=10
-seqs=simple_seqgen(n,N)
-print(make_pwm(seqs,n))
-'''
+	
 
 
 
--- models.py
+++ models.py
@@ -11,17 +11,7 @@ for line in fp.readlines():
 	line=line.rstrip()
 	seqs.append(line)
 
+np.savetxt(sys.argv[2],ml.make_pwm(seqs,6,len(seqs),0.001))
 
-#np.savetxt(sys.argv[2],ml.make_pwm(seqs,6,len(seqs)))
-
-# re-write make_simple_pwm to output in ACGT order
-for i in ml.make_simple_pwm(seqs,6,len(seqs)):
-	print(i)
-
-'''
-file=open(sys.argv[2],'w')
-file.write(str(content))
-file.close()
-'''
 
 	
