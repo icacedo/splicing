f4348246bdbc0444b27b5817320d511bea0345e1
izzy <icacedo@ucdavis.edu>
2021-11-19 15:09:24 -0800

    slow progress

--- models.py
+++ models.py
@@ -6,10 +6,11 @@ import numpy as np
 import isoform as iso
 import seqlib as sql
 
+seqs = []
+labels = []
 for label, seq in sql.read_fasta(sys.argv[1]):
-	print(label, seq)
-
-
+	seqs.append(seq)
+	labels.append(label)
 
 #### re-write of the api algorithm, with more parameters ######################
 # avg exon size in C. elegans: 200.7bp
@@ -19,8 +20,35 @@ for label, seq in sql.read_fasta(sys.argv[1]):
 # largest introns: 100912bp and 21230bp
 # use GU/AG or GT/AG rule for donor/acceptor sites
 # are the sequences in data/sequences/ translated mRNA or cDNA?
-for i in itertools.combinations([1,2,3,4],2):
-	print(i)
+# need to include min intron, min exon, max splice, flank
+
+# test seq is 43bp
+seqs = ['ATATATCGTCGATCAGTCGATCGGTACACCTGGAGTTCACATT']
+def splice_sites(seqs):
+
+	don_sites = []
+	acc_sites = []
+	for i in range(len(seqs)):
+		for j in range(len(seqs[i])-1):
+			if seqs[i][j:j+2]=='GT':
+				don_sites.append(j+1)
+			if seqs[i][j:j+2]=='AG':
+				acc_sites.append(j+2)
+			else: continue
+
+	return don_sites, acc_sites
+
+# need to filter after/during/while using the API algorithm
+
+'''
+for d in d_sites(seqs):
+	for a in a_sites(seqs):
+		print(d-a)
+'''
+
+
+#for i in itertools.combinations([1,2,3,4],2):
+#	print(i)
 
 
 
