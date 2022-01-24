65ed57c17b68d717cd75399c7cde1221c8e2e9b8
izzy <icacedo@ucdavis.edu>
2022-01-24 14:48:14 -0800

    update; working on filtering

--- models.py
+++ models.py
@@ -4,13 +4,13 @@ import itertools as it
 import modelib as ml
 import isoform as iso
 import seqlib as sql
-
+'''
 seqs = []
 labels = []
 for label, seq in sql.read_fasta(sys.argv[1]):
 	seqs.append(seq)
 	labels.append(label)
-
+'''
 #### re-write of the api algorithm, with more parameters ######################
 # avg exon size in C. elegans: 200.7bp
 # median: 123bp, smallest: 7bp, largest 7569bp
@@ -22,71 +22,89 @@ for label, seq in sql.read_fasta(sys.argv[1]):
 # need to include min intron, min exon, max splice, flank
 
 # test seq is 40bp, same 20 bp sequence twice
+#########1######8######15##19#######28#####35##39#####
+#########1######GT#####AG##GT#######GT#####AG##GT#####
 seqs = ['ATATATCGTCGATCAGCCGTATATATCGTCGATCAGCCGT']
 def splice_sites(seqs):
 
-	don_sites = []
-	acc_sites = []
+	seq_don_sites = []
+	seq_acc_sites = []
 	seq_lens = []
 	for i in range(len(seqs)):
-		for j in range(len(seqs[i])-1):
+		d_sites = []
+		a_sites = []
+		for j in range(len(seqs[i])):
 			if seqs[i][j:j+2]=='GT':
-				don_sites.append(j+1)
+				d_sites.append(j+1)
 			if seqs[i][j:j+2]=='AG':
-				acc_sites.append(j+2)
+				a_sites.append(j+1)
 			else: continue
+		seq_don_sites.append(d_sites)
+		seq_acc_sites.append(a_sites)
 		seq_lens.append(len(seqs[i]))	
 		
-	return don_sites, acc_sites, seq_lens
+	return seq_don_sites, seq_acc_sites, seq_lens
 
 # need to filter after/during/while using the API algorithm
 # flanking regions are not a part of the exon
 
-flank5 = 5
-minintron = 8
-minexon = 7
+# should flanking region be the same as minimum exon length?
+flank5 = 6
+flank3 = 5
+minintron = 3
+minexon = 5
+
 sites = splice_sites(seqs)
-d_sites = sites[0]
-a_sites = sites[1]
+don_sites = sites[0]
+acc_sites = sites[1]
 s_lens = sites[2]
 
-#print(d_sites, a_sites)
-
-for i in range(1,len(d_sites)):
-	for d in it.combinations(d_sites,i):
-		for a in it.combinations(a_sites,i):
-			# d and a are of equal length
-			# can use same index to get interior exons
-			#print(d,a)
-			for don, acc in zip(d,a):
-				# remove splice site if the donor comes after the acceptor
-				if don >= acc: continue
-				# remove short introns
-				i_len = acc-don
-				if i_len < minintron: continue
+for i in range(len(s_lens)):
+	for l in range(1,len(don_sites[i])+1):
+		for d in it.combinations(don_sites[i],l):
+			for a in it.combinations(acc_sites[i],l):
+			
+				# d and a are of equal length
+				# why are unequal values of l ignored?
+				# it.combinations() returns nothing if r > len(iterator)
+				# if a don/acc list larger than the other, those iterations
+				# are probably still done in the background
+				# will this affect performance? idk yet
+				# can use same index to get interior exons
+				
 				# remove short 5' exon
-				e_len5 = don - flank5
-				if e_len5 < minexon: continue
+				if d[0] < flank5: continue
+				
 				# remove short 3' exon
-				e_len3 = s_lens[i-2] - acc
-				if e_len3 < minexon: continue
-			for j in range(len(d)):	
-				print(j)
-				print(d[j], a[j])				
+				e_len3 = s_lens[i] - a[-1]
+				if e_len3 < flank3: continue
+			
+				for don, acc in zip(d,a):
+					print(don, acc)
+		
+					# remove splice site if the donor comes after the acceptor
+					#if don >= acc: continue
+					
+					# remove short introns
+					#i_len = acc-don
+					#if i_len < minintron: continue
+			
+			#for j in range(len(d)):	
+				#print(j)
+				#print(d[j], a[j])				
 			
 				
-				'''
-				if i_len < minintron: continue
-				e_len = 
-				'''
+				#if i_len < minintron: continue
+				#e_len = 
+				
 				# think
 				# if there are only one donor and acceptor sites
 				# the exons will be on the flanks
 				# does the min exon size
 				# work for only one of the exons on the flank?
-				
-			
 		
+			
+	
 
 
 
