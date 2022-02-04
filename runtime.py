92277081b56f3d2fe273391bb296f69e2d14acf2
AlanAloha <zby991023@gmail.com>
2022-02-04 13:14:44 -0800

    update

--- runtime.py
+++ runtime.py
@@ -2,6 +2,12 @@ import time
 import seqlib
 import sys
 import os
+'''
+To filter out the sequences that too long to run using the APC algorithm 
+
+To create a dataset with quickrunning data files
+'''
+fh = open('summary.tsv', 'w')
 
 for name, seq in seqlib.read_fasta(sys.argv[1]):
 	with open('temp.fa', 'w') as fp:
@@ -12,8 +18,17 @@ for name, seq in seqlib.read_fasta(sys.argv[1]):
 	t0 = time.time()
 	os.system('./geniso temp.fa | head -7 > temp.out')	
 	t1 = time.time()
-	print(t1-t0)
+	t = t1 - t0
 	
 	with open('temp.out') as fp:
 		lines = fp.readlines()
-		name = lines[0]
+		name = lines[0].split()[2]
+		length = lines[1].split()[2]
+		isoforms = lines[5].split()[2]
+	
+	fh.write(f'{name}	')
+	fh.write(f'{length}	')
+	fh.write(f'{isoforms}	')
+	fh.write(f'{str(t)}	\n')
+	
+fh.close()
