f751cd2b9d9c150f010d7f994f61030a053df44f
Ian Korf <ifkorf@gmail.com>
2022-02-05 10:45:50 -0800

    changes

--- runtime.py
+++ runtime.py
@@ -1,34 +1,30 @@
-import time
+import argparse
+import os
 import seqlib
 import sys
-import os
-'''
-To filter out the sequences that too long to run using the APC algorithm 
+import time
 
-To create a dataset with quickrunning data files
-'''
-fh = open('summary.tsv', 'w')
+parser = argparse.ArgumentParser(description='Run APC and get stats')
+parser.add_argument('apc', type=str,
+	metavar='<apc>', help='path to apc program')
+parser.add_argument('fasta', type=str,
+	metavar='<fasta>', help='path to fasta file')
+arg = parser.parse_args()
 
-for name, seq in seqlib.read_fasta(sys.argv[1]):
+for name, seq in seqlib.read_fasta(arg.fasta):
 	with open('temp.fa', 'w') as fp:
-		fp.write('>')
-		fp.write(name)
-		fp.write('\n')
-		fp.write(seq)
+		fp.write(f'>{name}\n{seq}\n')
 	t0 = time.time()
-	os.system('./geniso temp.fa | head -7 > temp.out')	
+	os.system(f'{arg.apc} temp.fa | head -7 > temp.out')	
 	t1 = time.time()
 	t = t1 - t0
 	
 	with open('temp.out') as fp:
 		lines = fp.readlines()
 		name = lines[0].split()[2]
-		length = lines[1].split()[2]
-		isoforms = lines[5].split()[2]
+		nts  = lines[1].split()[2]
+		dons = lines[2].split()[2]
+		accs = lines[3].split()[2]
+		isos = lines[5].split()[2]
 	
-	fh.write(f'{name}	')
-	fh.write(f'{length}	')
-	fh.write(f'{isoforms}	')
-	fh.write(f'{str(t)}	\n')
-	
-fh.close()
+	print(f'{name}\t{nts}\t{dons}\t{accs}\t{isos}\t{t:.2f}')
