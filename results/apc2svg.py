44eaa713cbba6030e48943416458610a340aea14
Ian Korf <ifkorf@ucdavis.edu>
2022-04-23 13:56:47 -0700

    hot

--- results/apc2svg.py
+++ results/apc2svg.py
@@ -10,10 +10,15 @@ from grimoire.genome import Reader
 
 def draw_transcript(tx, y):
 	body = []
+	if tx.score == '.': text = 'WormBase'
+	else:               text = tx.score
+	body.append(f'<text x="20" y="{y+10}" class="sm">{text}</text>')
 	for exon in tx.exons:
 		body.append(draw_exon(exon, y))
 	for intron in tx.introns:
 		body.append(draw_intron(intron, y))
+	for cds in tx.cdss:
+		body.append(draw_cds(cds, y))
 	for utr in tx.utr5s:
 		body.append(draw_utr(utr, y))
 	for utr in tx.utr3s:
@@ -38,11 +43,19 @@ def draw_intron(f, y):
 	else:               c = 'stroke="#f0f"'
 	return f'<line {x1} {y1} {x2} {y2} {c}/>'
 
+def draw_cds(f, y):
+	x = f'x="{f.beg+1}"';
+	y = f'y="{y+1}"'
+	w = f'width="{f.length-1}"'
+	h = 'height="10"'
+	c = 'fill="#ff0"'
+	return f'<rect {x} {y} {w} {h} {c}/>'
+
 def draw_utr(f, y):
-	x = f'x="{f.beg}"';
-	y = f'y="{y}"'
-	w = f'width="{f.length}"'
-	h = 'height="12"'
+	x = f'x="{f.beg+1}"';
+	y = f'y="{y+1}"'
+	w = f'width="{f.length-1}"'
+	h = 'height="10"'
 	c = 'fill="brown"'
 	return f'<rect {x} {y} {w} {h} {c}><title>{f.score}</title></rect>'
 
@@ -55,15 +68,38 @@ def draw_splice(f, y):
 	else:               c = 'fill="#f0f"'
 	return f'<rect {x} {y} {w} {h} {c}><title>{f.score}</title></rect>'
 
+def get_isoforms(fasta,  params):
+	os.system(f'isoformer {fasta} {params} > tmp.gff')
+	genome = Reader(gff='tmp.gff', fasta=fasta)
+	chrom = next(genome)
+	for gene in chrom.ftable.build_genes():
+		for tx in gene.transcripts():
+			tx.use_longest_orf()
+			yield tx
 
-# CLI
-parser = argparse.ArgumentParser(
-	description='apc image maker')
-parser.add_argument('apc', type=str, metavar='<apc dir>',
-	help='directory with apc set')
-parser.add_argument('out', type=str, metavar='<out dir>',
-	help='output directory')
-arg = parser.parse_args()
+# Hard-coded badness
+apc = 'apc'      # dir of fasta and gff
+tsv = 'apc.tsv'  # file of weights
+out = 'svg'      # outputdir
+mod = '../data'  # directory where models are
+
+# Isoformer params
+params = {}
+fitness = {}
+with open(tsv) as fp:
+	header = fp.readline()
+	for line in fp.readlines():
+		seq, don, acc, emm, imm, elen, ilen, cost, fit = line.split()
+		params[seq] = [
+			'--dpwm',  f'{mod}/donor.pwm',    f'--wdpwm {don}',
+			'--apwm',  f'{mod}/acceptor.pwm', f'--wapwm {acc}',
+			'--emm',   f'{mod}/exon.mm',      f'--wemm {emm}',
+			'--imm',   f'{mod}/intron.mm',    f'--wimm {imm}',
+			'--elen',  f'{mod}/exon.len',     f'--welen {elen}',
+			'--ilen',  f'{mod}/intron.len',   f'--wilen {ilen}',
+			'--icost', f'{cost}'
+		]
+		fitness[seq] = fit
 
 # Text
 style = '''
@@ -73,22 +109,32 @@ style = '''
 </style>
 '''
 
-for ff in glob.glob(f'{arg.apc}/*.fa'):
+# Main
+for ff in glob.glob(f'{apc}/*.fa'):
 	gf = ff[:-2] + 'gff3'
+	ch = ff[4:-3]
+	print(f'building svg for {ch}', file=sys.stderr)
 
 	genome = Reader(gff=gf, fasta=ff)
 	chrom = next(genome)
 
 	body = [] # body of SVG
+	
+	## Title
 	off = 20 # offset
 	body.append(f'<rect fill="#ddd" x="0" y="0" width="100%" height="100%"/>')
 	body.append(f'<text x="20" y="20" class="lg">{chrom.name} {chrom.desc}</text>')
 
+	## Transcripts
 	for gene in chrom.ftable.build_genes():
 		for tx in gene.transcripts():
 			off += 20
 			body.append(draw_transcript(tx, off))
 
+	## Fitmess
+	body.append(f'<text x="20" y="{off+30}" class="sm">Fitness: {fitness[ch]}</text>')
+
+	## RNASeq_splice (should scale by score)
 	rss = []
 	for f in chrom.ftable.features:
 		if f.source == 'RNASeq_splice':
@@ -98,10 +144,16 @@ for ff in glob.glob(f'{arg.apc}/*.fa'):
 		off += 5
 		body.append(draw_splice(splice, off))
 
-	xmlns = 'xmlns="http://www.w3.org/2000/svg"'
-	vbox = f'viewBox="0 0 {len(chrom.seq)} {500}"'
+	## Predicted isoforms & CDS
+	for iso in get_isoforms(ff, ' '.join(params[ch])):
+		off += 20
+		body.append(draw_transcript(iso, off))
 	
-	with open(f'{arg.out}/{chrom.name}.svg', 'w') as fp:
+
+	xmlns = 'xmlns="http://www.w3.org/2000/svg"'
+	vbox = f'viewBox="0 0 {len(chrom.seq)} {off+40}"'
+	
+	with open(f'{out}/{chrom.name}.svg', 'w') as fp:
 		fp.write(f'<svg {vbox} {xmlns}>\n')
 		fp.write(style)
 
@@ -109,8 +161,3 @@ for ff in glob.glob(f'{arg.apc}/*.fa'):
 			fp.write(line)
 			fp.write('\n')
 		fp.write('</svg>\n')
-
-	#sys.exit()
-
-
-	
