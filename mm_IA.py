4a85b6df829e0d0b655ccba905fdc5ff69a60afb
ismael <icacedo@ucdavis.edu>
2022-06-29 12:22:26 -0700

    mm example

--- mm_IA.py
+++ mm_IA.py
@@ -1,3 +1,21 @@
-# nth order mm is the same thing as a markov chain
-# consider a rotating mm? for coding frames, but more of a translation problem?
-# translation vs splicing
+s = 'ATGGCTCGATGCTAAGTATGGCTCGATGCT'
+k = 3
+
+kmers = {}
+mm = {}
+for n in range(len(s)-2):
+	ctx = s[n:n+2]
+	nt = s[n+k-1]
+	print(ctx,nt)
+	if s[n:n+k] not in kmers:
+		kmers[s[n:n+k]]=1
+	else:
+		kmers[s[n:n+k]]+=1
+	if ctx not in mm:
+		mm[ctx] = {}
+	if nt not in mm[ctx]:	
+		mm[ctx][nt] = 0
+	mm[ctx][nt]+=1
+print(mm)
+print(kmers)
+
