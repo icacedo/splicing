53959fb5c9b286b0702ff4e03ef4880a690c68c8
ismael <icacedo@ucdavis.edu>
2022-07-26 18:46:32 -0700

    update

--- homologues/BLAST_tutorial.py
+++ homologues/BLAST_tutorial.py
@@ -2,6 +2,9 @@
 # Pt. 1: https://jnmaloof.github.io/BIS180L_web/2022/04/05/2-BLAST-part1/
 # Pt. 2: https://jnmaloof.github.io/BIS180L_web/2022/04/07/BLAST-part2/
 
+# cp from a list in current directory to another directory:
+# ls | head | xargs -i cp {} /directory
+# used to get 5 .fa and .gff3 files from the apc dataset to test on
 
 
 
