a03890a5590e079b3dc33640365fce6c8e3f2c26
ismael <icacedo@ucdavis.edu>
2022-08-16 15:36:02 -0700

    update

--- homologues/APCslator.py
+++ homologues/APCslator.py
@@ -10,8 +10,9 @@ import os
 import sys
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-gff3', type=str)
-parser.add_argument('-fasta', type=str)
+parser.add_argument('--gff3', type=str)
+parser.add_argument('--fasta', type=str)
+parser.add_argument('--path', type=str)
 
 arg = parser.parse_args()
 
@@ -52,41 +53,49 @@ def get_CDS_regions(CDS_lines):
 		CDS_regions[line[0]] = starts_n_ends
 
 	return CDS_regions
+
+def get_CDS_sequences(CDS_regions, fasta):
+
+	CDS_sequences = {}
+	for ID, seq in seqlib.read_fasta(arg.fasta):
+		ID = ID.split()
+		ch_id = ID[0]
+		CDS_seq = ''
+		for CDS in CDS_regions[ch_id]:
+			start = int(CDS[0])
+			end = int(CDS[1])
+			CDS_seq += seq[start-1:end]
+
+		CDS_sequences[ch_id] = CDS_seq
 	
+	return CDS_sequences
+
 CDS_lines = gff_reader(arg.gff3)
 
 CDS_regions = get_CDS_regions(CDS_lines)
 
-CDS_sequences = {}
+CDS_sequences = get_CDS_sequences(CDS_regions, arg.fasta)
 
-for ID, seq in seqlib.read_fasta(arg.fasta):
-	ID = ID.split()
-	ch_id = ID[0]
-	CDS_seq = ''
-	for CDS in CDS_regions[ch_id]:
-		start = int(CDS[0])
-		end = int(CDS[1])
-		CDS_seq += seq[start-1:end]
-
-	CDS_sequences[ch_id] = CDS_seq
-	
 print(CDS_sequences)
 
+filename = 'C_elegans.APC.CDS.fa'
 
 for ID in CDS_sequences:
-	name = ID + '.CDS.fa'
 	seq = CDS_sequences[ID]
-	with open(name, 'w') as fo:	
+	with open(filename, 'w') as fo:	
 		fo.write(ID+'.CDS'+'\n')
 		lines = []
 		for i in range(0, len(seq), 80):
 			line = seq[i:i+80]
 			lines.append(line)
-			print(seq[i:i+80])
 		fo.write('\n'.join(lines))
 
 # can put multiple sequences in the same file for command line blast
 # also, do i need to worry about the phase?
+# used this command for all gff files in the apc/ directory
+# awk '//{print $8}' data/apc/*.gff3 | sort --unique
+# there only '.'
+# i will just run blastx for now, it doesn't need a specific start site
 
 '''
 sss = 'ACTGTGACTG'
@@ -99,6 +108,10 @@ with open('test', 'w') as foo:
 	foo.write('\n'.join(lines))
 '''
 		
+import os
+
+print(arg.path)
+
 
 
 
