211ebb5c2e13308c3323d2e2b9a7da8934ca9e53
icacedo <icacedo@ucdavis.edu>
2022-10-28 17:12:31 -0700

    update

--- mrnaseq_example/swap.py
+++ mrnaseq_example/swap.py
@@ -1,4 +1,8 @@
+# goal is to swap the gene_id identfier position in 1pct.gffread.gtf
+# to see if works with subread
+
 import sys
+import re
 
 f = sys.argv[1]
 fp = open(f)
@@ -6,6 +10,13 @@ fp = open(f)
 count = 0
 for line in fp.readlines():
 	line = line.split('\t')
-	print(line)
+	string = line[8]
+	#print(string)
+	strings = re.split('(;)', string)
+	print(strings)
+	#transcript_id = strings[0] + strings[1]
+	#gene_id = strings[2] + strings[3]
+	#print(transcript_id)
+	#print(gene_id) 
 	count += 1
-	if count == 10: sys.exit()
+	if count == 2: sys.exit()
