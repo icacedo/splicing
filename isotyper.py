4898ab59fb86ca1f4643bf331ad57246df3accac
izzy <icacedo@ucdavis.edu>
2023-03-08 17:23:09 -0800

    nothing

--- isotyper.py
+++ isotyper.py
@@ -4,13 +4,24 @@
 
 import seqlib
 import sys
+import os
+from grimoire.genome import Reader
 
 fasta = sys.argv[1]
-apc_info = 
+#apc_info = 
 
-for i,j in seqlib.read_fasta(fasta):
-    print(i,j)
+#for i,j in seqlib.read_fasta(fasta):
+#    print(i,j)
 
+# where does intron information come from?
+# copy pasted from apc2html.py
 
-    
+def get_isoforms(fasta,  params):
+	os.system(f'isoformer {fasta} {params} > tmp.gff')
+	genome = Reader(gff='tmp.gff', fasta=fasta)
+	chrom = next(genome)
+	for gene in chrom.ftable.build_genes():
+		for tx in gene.transcripts():
+			tx.use_longest_orf()
+			yield tx
 
