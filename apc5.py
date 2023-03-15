432bc246eab08ee8096280233e7dbd73a3f686f1
izzy <icacedo@ucdavis.edu>
2023-03-15 02:13:35 -0700

    nope

--- apc5.py
+++ apc5.py
@@ -1,3 +1,5 @@
+import sys
+
 introns = [(3,4),(5,6),(7,8)]
 
 def count_down(n):
@@ -5,27 +7,39 @@ def count_down(n):
     if n>0:
         count_down(n-1)
 
-count_down(5)
-
-
-tator2 = iter([x for x in introns[1:]])
-print([y for y in tator2])
+#tator2 = iter([x for x in introns[1:]])
+#print([y for y in tator2])
 
 # this works
-def afunc(introns):
+def afunc(introns):	
 	print(introns)
-	if len(introns)>1:
-		afunc(introns[1:])
+	if len(introns) > 1:
+		return afunc(introns[1:])
 
 afunc(introns)
 
+print('***')
+# you don't need a recursive function lol
+#for i in range(1, len(introns)):
+#	print(introns[i:])
 
-'''
-def afunc(introns):
-	print(introns)	
-	if len(introns) > 1:
-		tator = iter([x for x in introns[1:]])
-		afunc(tator)
-			
-print(afunc(introns))
-'''
+def funk(introns):
+	for i in range(1, len(introns)):
+		yield introns[i]
+
+for i in funk(introns):
+	print(i)
+
+for i in introns[0]:
+	print(i)
+	for j in funk(introns):
+		for k in j:
+			print(i,k)
+
+tator = iter([x for x in introns])
+print(next(tator))
+for i in next(tator):
+	print(i)
+
+
+
