562f73f8bac58217dfaf3b4d6e24e99ca4750b6c
izzy <icacedo@ucdavis.edu>
2023-05-03 15:11:35 -0700

    up

--- modelib.py
+++ modelib.py
@@ -2,12 +2,13 @@ import sys
 import gzip
 import openturns as ot
 import math
+from itertools import combinations
 
 # unit testing in python
 # https://www.dataquest.io/blog/unit-tests-python/
 
 # for testing
-fp = sys.argv[1]
+#fp = sys.argv[1]
 
 ########################################
 ##### Begin Length Model Section #######
@@ -172,13 +173,111 @@ def memoize_fdist(fp, nbins=None, prec=None, size_limit=250):
 	# only scores are useful?
 	return y_scores
 
-print(memoize_fdist(fp))
+#print(memoize_fdist(fp))
 
 
 
 ########################################
 ##### End Length Model Section #########
 ########################################
+
+########################################
+##### Begin APC Section ################
+########################################
+
+def get_gtag(seq):
+
+	dons = []
+	accs = []
+	for i in range(len(seq)):
+		if seq[i:i+2] == 'GT':
+			dons.append(i)
+		if seq[i:i+2] == 'AG':
+			accs.append(i+1)
+
+	return dons, accs
+
+# using index starting at 0
+def short_introns(dons, accs, minin):
+
+	for d, a in zip(dons, accs):
+		intron_length = a - d + 1
+		if intron_length < minin:
+			return True
+
+	return False
+
+# using index starting at 0
+def short_exons(dons, accs, flank, minex, seq):
+
+	# check 5' first exon
+	fexlen = dons[0] - flank
+	if fexlen < minex:
+		return True
+
+	# check 3' last exon
+	lexbeg = accs[-1] + 1
+	lexend = len(seq) - flank - 1 
+	lexlen = lexend - lexbeg + 1
+	if lexlen < minex:
+		return True
+
+	# check interior exons
+	for i in range(len(dons)-1):
+		if dons[i+1] - accs[i] - 1 < minex:
+			return True
+
+	return False	
+
+def get_exons(dsites, asites, flank, seq):
+
+	exons = []
+	exons.append((flank, dsites[0]-1))
+	for i in range(1, len(dsites)):
+		exbeg = asites[i-1] + 1
+		exend = dsites[i] - 1
+		exons.append((exbeg, exend))
+	exons.append((asites[-1]+1, len(seq)-flank-1))
+
+	return exons	
+
+def get_introns(dsites, asites):
+
+	introns = []	
+	for d, a in zip(dsites, asites):	
+		introns.append((d, a))
+
+	return introns
+
+def apc(dons, accs, maxs, minin, minex, flank, seq):
+
+	apc_isoforms = []
+
+	apc_isoform = {
+		'seq': '',
+		'beg': '',
+		'end': '',
+		'exons': [],
+		'introns': [],
+		'score': 0
+	}	
+
+	trials = 0
+	short_introns_exons = 0
+	nsites = min(len(dons), len(accs), maxs)
+	for n in range(1, nsites+1):
+		for dsites in combinations(dons, n):
+			for asites in combinations(accs, n):
+				trials += 1
+				if short_introns(dsites, asites, minin): continue
+				if short_exons(dsites, asites, flank, minex, seq): continue
+				apc_isoform['seq'] = seq
+				apc_isoform['beg'] = flank
+				apc_isoform['end'] = len(seq) - flank - 1
+				apc_isoform['exons'] = get_exons(dsites, asites, flank, seq)
+				apc_isoform['introns'] = get_introns(dsites, asites)
+				print(apc_isoform)
+	print(trials)
 
 
 
--- testing.py
+++ testing.py
@@ -1,6 +1,8 @@
 import modelib as ml
 import sys
+import isoform
 
+'''
 fp = sys.argv[1]
 
 intbins = ml.get_intbins(fp)[0]
@@ -8,6 +10,37 @@ intbins = ml.get_intbins(fp)[0]
 print(ml.rec_smoo(intbins))
 
 print(ml.tri_smoo(intbins))
+'''
+#       0        9      16    22       31     38       47  
+#                D      A     D        A      A          
+seq1 = 'AACATGACCGTTGCGAGCTACCGTCACATTAGCTCGGAGCCCTATATA'
+# iso1: AACATGACC        TTACCGTCACATTAGTTCGGAGCCCTATATA
+# iso2: AACATGACC                       TTCGGAGCCCTATATA
+# iso3: AACATGACC                              CCCTATATA
+# iso4: AACATGACCGTTGCGAGTTACC          TTCGGAGCCCTATATA
+# iso5: AACATGACCGTTGCGAGTTACC                 CCCTATATA
+# iso6: AACATGACC        TTACC          TTCGGAGCCCTATATA
+# iso7: AACATGACC        TTACC                 CCCTATATA
 
+'''
+defaults:
+maxs 100
+minin 25
+minex 25
+flank 100
+'''
 
+minin = 3
+minex = 4
+flank = 5
+maxs = 100
+
+dons, accs = ml.get_gtag(seq1)
+print(dons)
+print(accs)
+ml.apc(dons, accs, maxs, minin, minex, flank, seq1)
+
+fp = sys.argv[1]
+name, seq = next(isoform.read_fasta(fp))
+print(name, seq)
 
