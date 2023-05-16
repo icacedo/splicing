213026b656c181804ee493f74ba4f68400777c34
izzy <icacedo@ucdavis.edu>
2023-05-16 16:46:06 -0700

    update

--- dev_gff_reader.py
+++ dev_gff_reader.py
@@ -17,6 +17,8 @@ faf.close()
 
 print(seq)
 
+dons = []
+accs = []
 with open(sys.argv[1]) as gff:
 	while True:
 		line = gff.readline()
@@ -25,13 +27,26 @@ with open(sys.argv[1]) as gff:
 		fields = line.split('\t')
 		if fields[2] == 'intron':
 			print(line)
-			beg = int(fields[3])
-			end = int(fields[4])
+			beg = int(fields[3]) - 1
+			end = int(fields[4]) - 1
 			print(beg, end)
-			print(seq[beg-1:end])
-			print(seq[beg-5:end+5])
+			dons.append(beg)
+			accs.append(end)
+			print(seq[beg:end+1])
+			print(seq[beg:beg+2]) # get GT
+			print(seq[end-1:end+1]) # get AG
+	
+gff.close()	
 
-gff.close()
+import modelib
+
+print(dons, accs)
+print('***')
+dons1, accs1 = modelib.get_gtag(seq)
+print(dons1, accs1)
+
+# next test ian's code
+
 
 
 
