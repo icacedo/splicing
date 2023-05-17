0fb3b26bf090f4ab2dec360de346b3ecf7691fd4
izzy <icacedo@ucdavis.edu>
2023-05-17 13:07:16 -0700

    working

--- dev_gff_reader.py
+++ dev_gff_reader.py
@@ -1,52 +1,51 @@
 import sys
+import modelib
+import isoform
 
 # get gt/ag sites from gff file
 # use ch.9940
 
-seq = ''
-with open(sys.argv[2]) as faf:
-	while True:
-		line = faf.readline()
-		line = line.rstrip()
-		if not line: break
-		print(line)
-		if line.startswith('>'): continue
-		seq += line
-		
-faf.close()		
+def read_fastas(fastafile):
 
-print(seq)
+	with open(fastafile) as fp:
 
-dons = []
-accs = []
-with open(sys.argv[1]) as gff:
-	while True:
-		line = gff.readline()
-		line = line.rstrip()
-		if not line: break
-		fields = line.split('\t')
-		if fields[2] == 'intron':
-			print(line)
-			beg = int(fields[3]) - 1
-			end = int(fields[4]) - 1
-			print(beg, end)
-			dons.append(beg)
-			accs.append(end)
-			print(seq[beg:end+1])
-			print(seq[beg:beg+2]) # get GT
-			print(seq[end-1:end+1]) # get AG
-	
-gff.close()	
+		seqid = ''
+		seq = ''	
 
-import modelib
+		for line in fp.readlines():
+			line = line.rstrip()
+			if line.startswith('>'): seqid += line
+			else: seq += line
+		yield (seqid, seq)			
+	fp.close()		
 
-print(dons, accs)
-print('***')
-dons1, accs1 = modelib.get_gtag(seq)
-print(dons1, accs1)
+def gff_sites(seq, gff, gtag=True):
 
-# next test ian's code
+	dons = []
+	accs = []
 
+	with open(gff) as fp:
+		while True:
+			line = fp.readline()
+			line = line.rstrip()
+			if not line: break
+			fields = line.split('\t')
+			if fields[2] == 'intron':
+				beg = int(fields[3]) - 1
+				end = int(fields[4]) - 1
+				if gtag:
+					if seq[beg:beg+2] == 'GT': dons.append(beg)
+					if seq[end-1:end+1] == 'AG': accs.append(end)
+	fp.close()	
+	return sorted(set(dons)), sorted(set(accs))
+
+for i in read_fastas(sys.argv[2]):
+	dons, accs = gff_sites(i[1], sys.argv[1])
+	print(dons, accs)
+	dons1, accs1 = modelib.get_gtag(i[1])
+	print(dons1, accs1)	
+	dons2, accs2 = isoform.gff_sites(i[1], sys.argv[1])
+	print(dons2, accs2)	
 
 
 
