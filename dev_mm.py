02809a9c881814e48fc30031f82ef294449e563c
izzy <icacedo@ucdavis.edu>
2023-05-18 18:05:19 -0700

    it works

--- dev_mm.py
+++ dev_mm.py
@@ -3,33 +3,51 @@ import isoform
 import sys
 import math
 
+exin_seqs = []
+with open(sys.argv[1], 'r') as fp:	
+	exin_seq = ''
+	for line in fp.readlines():
+		line = line.rstrip()
+		exin_seq = line
+		exin_seqs.append(exin_seq)
 
+
+seqs = [
+	'CTTGGACATATCGCATATCGATTTGCTATTCCGGATGCTAATGAGCTCCGCCATTACGCCGCTGGATCATCAACC', 
+	'TTGCCAACCAACGGAGTTGTCGCAGACGAAAGAAAGTATACATCTACAAGCTGAACGACAGGCTCACACAGTGCGGAATG', 
+	'ATGTCTCTTAAGATTTCTTATTTTCGAACACGTTGCACATCCGCAGAACCTTGAAAGAGCTTCTGGAAGT'
+]
 s = 'ATGGGCGCGTTATATGGCTACGATAATATCGACTATC'
 #s = 'ACTGACTGAC'
 order = 3
 
+
 context = {}
-for i in range(len(s)-order):
-	prev = s[i:i+order]
-	now = s[i+order]
-	print(prev, now)
-	if prev not in context:
-		context[prev] = now
-	else:
-		context[s[i:i+order]] += now
-print(context)
+for seq in exin_seqs:
+	for i in range(len(seq)-order):
+		prev = seq[i:i+order]
+		now = seq[i+order]
+		if prev not in context:
+			context[prev] = now
+		else:
+			context[seq[i:i+order]] += now
+#print(context)
 
-flist = []
-for i in context:
-	print(i, context[i])
-	for j in context [i]:
-		print(j)
-
-
-
-
-
-
+# this is correct compared to exon.mm
+for nts in sorted(context):
+	#print(nts, context[nts])
+	print(nts)
+	A = 0
+	C = 0
+	G = 0
+	T = 0
+	for nt in context[nts]:
+		if nt == 'A': A += 1
+		if nt == 'C': C += 1
+		if nt == 'G': G += 1
+		if nt == 'T': T += 1
+		d = int(len(context[nts]))
+	print(A/d, C/d, G/d, T/d)
 
 
 
