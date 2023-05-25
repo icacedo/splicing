dd72c010aae5ff35adb9aa74dd8d3dca1a8f88c5
carl <icacedo@ucdavis.edu>
2023-05-25 15:01:04 -0700

    pfm

--- dev_pwm.py
+++ dev_pwm.py
@@ -3,10 +3,15 @@ import sys
 
 seqs = ml.read_txt_seqs(sys.argv[1])
 
-pfm = [{}for x in range(len(seqs[0]))]
+seqs = [
+	'ACTCAA',
+	'CGTACG',
+	'CTAGTG'
+]
+
+pfm = [{'A': 0, 'C': 0, 'G': 0, 'T':0} for x in range(len(seqs[0]))]
+for i in range(len(seqs)):
+	for j in range(len(seqs[i])):
+		pfm[j][seqs[i][j]] += 1
+
 print(pfm)
-
-for s in seqs:
-	for i in range(len(s)):
-		print(s[i])
-
