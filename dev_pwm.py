a6adebec418d14079b7204e03dabea18f2fd5592
carl <icacedo@ucdavis.edu>
2023-05-25 17:56:38 -0700

    update

--- dev_pwm.py
+++ dev_pwm.py
@@ -1,17 +1,28 @@
 import modelib as ml
 import sys
+import math 
 
 seqs = ml.read_txt_seqs(sys.argv[1])
 
 seqs = [
 	'ACTCAA',
 	'CGTACG',
-	'CTAGTG'
+	'CTAGTG',
+	'GTCGTA'
 ]
 
-pfm = [{'A': 0, 'C': 0, 'G': 0, 'T':0} for x in range(len(seqs[0]))]
+ppm = [{'A': 0, 'C': 0, 'G': 0, 'T':0} for x in range(len(seqs[0]))]
 for i in range(len(seqs)):
 	for j in range(len(seqs[i])):
-		pfm[j][seqs[i][j]] += 1
+		ppm[j][seqs[i][j]] += 1/len(seqs)
 
-print(pfm)
+pwm = [{'A': 0, 'C': 0, 'G': 0, 'T': 0} for x in range(len(ppm))]
+for i in range(len(ppm)):	
+	for n in ppm[i]:
+		if ppm [i][n] == 0:
+			pwm[i][n] = math.log2(0.0001) # pseudocount
+		else:
+			pwm[i][n] = math.log2(ppm[i][n])
+print(ppm)
+print(pwm)
+		
