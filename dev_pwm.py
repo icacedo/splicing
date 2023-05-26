8a4b37179dcbd54b67095d16e5074dbdd9e95311
izzy <icacedo@ucdavis.edu>
2023-05-26 11:42:46 -0700

    update

--- dev_pwm.py
+++ dev_pwm.py
@@ -3,26 +3,32 @@ import sys
 import math 
 
 seqs = ml.read_txt_seqs(sys.argv[1])
-
+'''
 seqs = [
 	'ACTCAA',
 	'CGTACG',
 	'CTAGTG',
 	'GTCGTA'
 ]
-
-ppm = [{'A': 0, 'C': 0, 'G': 0, 'T':0} for x in range(len(seqs[0]))]
+'''
+pfm = [{'A': 0, 'C': 0, 'G': 0, 'T':0} for x in range(len(seqs[0]))]
 for i in range(len(seqs)):
 	for j in range(len(seqs[i])):
-		ppm[j][seqs[i][j]] += 1/len(seqs)
-
+		pfm[j][seqs[i][j]] += 1
+print(pfm)
+print('*****')
+ppm = [{'A': 0, 'C': 0, 'G': 0, 'T':0} for x in range(len(pfm))]
+for i in range(len(pfm)):
+	for n in pfm[i]:
+		ppm[i][n] = pfm[i][n]/len(seqs)
+print(ppm)
+print('*****')
 pwm = [{'A': 0, 'C': 0, 'G': 0, 'T': 0} for x in range(len(ppm))]
 for i in range(len(ppm)):	
 	for n in ppm[i]:
 		if ppm [i][n] == 0:
-			pwm[i][n] = math.log2(0.0001) # pseudocount
+			pwm[i][n] = -100
 		else:
-			pwm[i][n] = math.log2(ppm[i][n])
-print(ppm)
+			pwm[i][n] = math.log2(ppm[i][n]/0.25)
 print(pwm)
 		
