835802974b718f3f012fc2deff91c4d013596286
izzy <icacedo@ucdavis.edu>
2023-06-02 16:10:17 -0700

    update

--- make_models.py
+++ make_models.py
@@ -7,7 +7,7 @@ parser = argparse.ArgumentParser(
 	description='Generates len, MM, and PWM models for apc')
 
 parser.add_argument('--extxt', type=str, metavar='<file>', 
-	help='input text file with exon sequences')
+	required=False, help='input text file with exon sequences')
 parser.add_argument('--intxt', type=str, metavar='<file>',
 	help='input text file with intron sequences')
 parser.add_argument('--dntxt', type=str, metavar='<file>',
@@ -16,36 +16,34 @@ parser.add_argument('--actxt', type=str, metavar='<file>',
 	help='input text file with acceptor site sequences')
 args = parser.parse_args()
 
-exons = ml.read_txt_seqs(args.extxt)
+def mm_tsv_write(exintxt):
 
-#exlen_scores, exlen_probs = ml.memoize_fdist(exons)
+	exins = ml.read_txt_seqs(exintxt)
 
-exmm_scores, exmm_probs = ml.make_mm(exons)
+	exinmm_scores, exinmm_probs = ml.make_mm(exins)
 
-for key in exmm_scores:
-	print(key + 'A', exmm_probs[key][0], exmm_scores[key][0])
-	print(key + 'C', exmm_probs[key][1], exmm_scores[key][1])
-	print(key + 'G', exmm_probs[key][2], exmm_scores[key][2])
-	print(key + 'T', exmm_probs[key][3], exmm_scores[key][3])
-	print('')
+	root, ext = args.extxt.split('.')
+	filename = root + '_mm' + '.tsv'
 
+	with open(filename, 'w', newline='') as tsvfile:
+		writer = csv.writer(tsvfile, delimiter='\t', lineterminator='\n')
+		for key in exinmm_scores:
+			writer.writerow([key + 'A', exinmm_probs[key][0], 
+				exinmm_scores[key][0]])
+			writer.writerow([key + 'C', exinmm_probs[key][1], 
+				exinmm_scores[key][1]])
+			writer.writerow([key + 'G', exinmm_probs[key][2], 
+				exinmm_scores[key][2]])
+			writer.writerow([key + 'T', exinmm_probs[key][3], 
+				exinmm_scores[key][3]])
+	tsvfile.close()
 
-root, ext = args.extxt.split('.')
-filename = root + '_mm' + '.tsv'
-print(filename)
+# input exon.txt output exon_mm.tsv
+#mm_tsv_write(args.extxt)
+# input intron.txt output intron_mm.tsv
+#mm_tsv_write(args.intxt)
 
-with open(filename, 'w', newline='') as tsvfile:
-	writer = csv.writer(tsvfile, delimiter='\t', lineterminator='\n')
-	for key in exmm_scores:
-		writer.writerow([key + 'A', exmm_probs[key][0], exmm_scores[key][0]])
-		writer.writerow([key + 'C', exmm_probs[key][1], exmm_scores[key][1]])
-		writer.writerow([key + 'G', exmm_probs[key][2], exmm_scores[key][2]])
-		writer.writerow([key + 'T', exmm_probs[key][3], exmm_scores[key][3]])
-tsvfile.close()
-
-
-#donors = ml.read_txt_seqs(args.dntxt)
-
-#don_pwm, don_ppm = ml.pwm_score(donors)
-
-
+if args.extxt:
+	mm_tsv_write(args.extxt)
+if args.intxt:
+	mm_tsv_write(args.intxt)
