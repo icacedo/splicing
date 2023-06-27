c2e7cae838893ae9d21b2f16448d05d49df059b8
carl <icacedo@ucdavis.edu>
2023-06-27 14:23:22 -0700

    update

--- pwm_scoring.py
+++ pwm_scoring.py
@@ -2,6 +2,7 @@ import sys
 import modelib as ml
 
 # use ch.9940.fa for testing
+'''
 fp = sys.argv[1]
 
 seqid = None
@@ -9,7 +10,7 @@ seq = None
 for seqid, seq in ml.read_fastas(fp):
 	seqid = seqid
 	seq = seq
-
+'''
 #      0        9      16    22       31     38       47  
 #               D      A     D        A      A          
 seq = 'AACATGACCGTTGCGAGCTACCGTCACATTAGCTCGGAGCCCTATATA'
@@ -50,23 +51,7 @@ def read_pwm(pwm_tsv):
 				re_pwm.append(line.split('\t'))
 	return re_ppm, re_pwm
 
-def get_donacc_seqs(apc_isoforms):
-
-	for iso in apc_isoforms:
-		#print(iso)
-		d_seqs = []
-		a_seqs = []
-		for intron in iso['introns']:
-			d_start = intron[0]
-			d_end = d_start + 5
-			a_end = intron[1] + 1
-			a_start = a_end - 6
-			d_seqs.append(seq[d_start:d_end])
-			a_seqs.append(seq[a_start:a_end])
-			#print(d_seqs, a_seqs)
-		yield iso, d_seqs, a_seqs
-
-def get_donacc_seqs2(isoform):
+def get_donacc_seqs(isoform):
 	
 	d_seqs = []
 	a_seqs = []
@@ -81,6 +66,7 @@ def get_donacc_seqs2(isoform):
 
 def get_pwm_score(da_seqs, da_pwm):
 
+	da_score_total = 0
 	for i in range(len(da_seqs)):
 		da_score = 0
 		for j in range(len(da_seqs[i])):
@@ -92,35 +78,26 @@ def get_pwm_score(da_seqs, da_pwm):
 				da_score += float(da_pwm[j][2])
 			if da_seqs[i][j] == 'T':
 				da_score += float(da_pwm[j][3])
-		yield da_seqs[i], da_score
+		da_score_total += da_score
+		#print(da_seqs[i], da_score)
+	return da_score_total
 
 dons, accs = ml.get_gtag(seq)
-#apc_isos, trials = ml.apc(dons, accs, maxs, minin, minex, flank, seq)
 apc_isoforms, trials = ml.apc(dons, accs, maxs, minin, minex, flank, seq)
 
+donor_pwm_tsv = sys.argv[1]
+acceptor_pwm_tsv = sys.argv[2]
+
+donor_ppm, donor_pwm = read_pwm(donor_pwm_tsv)
+acceptor_ppm, acceptor_pwm = read_pwm(acceptor_pwm_tsv)
 
 for iso in apc_isoforms:
+	d_seqs, a_seqs = get_donacc_seqs(iso)
+	dpwm_sc = get_pwm_score(d_seqs, donor_pwm)
+	apwm_sc = get_pwm_score(a_seqs, acceptor_pwm)
+	iso['score'] = dpwm_sc + apwm_sc
 	print(iso)
-	get_donacc_seqs2(iso)
 
-
-
-donor_ppm, donor_pwm = read_pwm(sys.argv[2])
-
-acceptor_ppm, acceptor_pwm = read_pwm(sys.argv[3])
-
-
-for iso, d_seqs, a_seqs in get_donacc_seqs(apc_isoforms):
-	print(iso['score'])
-	dscore_total = 0
-	ascore_total = 0
-	for d_seq, dscore in get_pwm_score(d_seqs, donor_pwm):
-		print(d_seq, dscore)
-		dscore_total += dscore
-	for a_seq, ascore in get_pwm_score(a_seqs, acceptor_pwm):
-		print(a_seq, ascore)
-		ascore_total += ascore
-	print(dscore_total, ascore_total)
 
 
 
