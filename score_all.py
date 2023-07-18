2a259c587edb6628e6bc4edd42820df419842ef1
izzy <icacedo@ucdavis.edu>
2023-07-18 15:04:43 -0700

    update

--- score_all.py
+++ score_all.py
@@ -1,6 +1,16 @@
-import sys
+import argparse
 import modelib as ml
 
+parser = argparse.ArgumentParser(
+	description='generate and score alternative isoforms')
+parser.add_argument('fasta', type=str, metavar='<file>',
+	help='input single sequence fasta file')
+parser.add_argument('--min_intron', required=False, type=int, default=25,
+	metavar='<int>', help='minimum length of intron [%(default)i]')
+# how does this work? and -help not working
+
+
+'''
 # ch.9940.fa
 fp = sys.argv[1]
 
@@ -11,3 +21,15 @@ for seqid, seq in ml.read_fastas(fp):
 	seq = seq
 print(seqid)
 print(seq)
+
+dons, accs = ml.get_gtag(seq)
+
+maxs = 100
+minin = 25
+minex = 25
+flank = 100
+
+apc_isoforms, trials = ml.apc(dons, accs, maxs, minin, minex, flank, seq)
+for iso in apc_isoforms:
+	print(iso)
+'''
