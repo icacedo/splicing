24c91eea98e229b0ac7f73205960368f04b9d9a8
izzy <icacedo@ucdavis.edu>
2023-10-03 16:26:24 -0700

    up

---
+++ icost/avg_mdist.py
@@ -0,0 +1,15 @@
+import argparse
+import json
+
+parser = argparse.ArgumentParser()
+parser.add_argument('json_file', type=str, metavar='<file>',
+	help='input json file with tested icosts')
+
+args = parser.parse_args()
+
+f = open(args.json_file)
+
+mdists = json.load(f)
+
+for icost in mdists:
+	print(icost)
