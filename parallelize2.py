83a5bd49a362ddc637e6c6fbcbe2cdcd2023235d
izzy <icacedo@ucdavis.edu>
2023-10-30 16:00:44 -0700

    up

--- parallelize2.py
+++ parallelize2.py
@@ -1,27 +1,43 @@
-#!/usr/bin/env python3
-
 # based on Ian's code in setup/bin/parallelize
 
 import argparse
-from multiprocessing import Pool
+import multiprocessing as mp
 import subprocess
 import os
+
+parser = argparse.ArgumentParser(description='command line parallelizer')
+parser.add_argument('file', help='file of command lines')
+parser.add_argument('--cpus', required=False, type=int, default=1,
+	metavar='<int>', help='number of CPUs to use [%(default)i]')
+
+args = parser.parse_args()
+
 
 def worker(cmd):
 	return subprocess.run(cmd, shell=True, capture_output=True).stdout.decode()
 
-def f(x):
-	return x*x
+jobs = []
+with open(args.file, 'r') as fp:
+	for cmd in fp:
+		jobs.append(cmd.rstrip())
 
+# this puts results in a list
+# can i do that for the resulting gffs, and put them in individual files?
+pool = mp.Pool(args.cpus)
+a = pool.map(worker, jobs)
+print(a)
+for i in a:
+	print(i)
+
+	
+'''
 with Pool(5) as p:
 	print(p.map(f, [1, 2, 3]))
-
-from multiprocessing import Process
 
 # go to codesdope.com intro to multiprocessing and process in python
 
 print(os.cpu_count())
-
+'''
 
 
 
