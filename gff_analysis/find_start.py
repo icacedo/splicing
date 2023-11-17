e8e3b1cd205b72d55adbb2cc1c64adce8c145de2
izzy <icacedo@ucdavis.edu>
2023-11-17 11:42:38 -0800

    up

--- gff_analysis/find_start.py
+++ gff_analysis/find_start.py
@@ -1,13 +1,11 @@
 import sys
 
-# use ch.7157
+# ch.7157 ATG lines up with wormbase
+# ch.9018 ATG lines up with wormbase
+# five_prime_UTR in gff is where the start codon is 
 
 gff = sys.argv[1]
 fasta = sys.argv[2]
-
-with open(gff, 'r') as fp:
-	for line in fp.readlines():
-		print(line.rstrip())
 
 with open(fasta, 'r') as fp:
 	seq = ''
@@ -18,9 +16,20 @@ with open(fasta, 'r') as fp:
 		else:
 			seq += line
 
+with open(gff, 'r') as fp:
+	for line in fp.readlines():
+		line = line.rstrip()
+		sline = line.split('\t')
+		#print(sline)
+		if sline[2] == 'five_prime_UTR':
+			print(sline)
+			start = int(sline[4])
+			print(seq[start:start+10], start) 
+
+'''
 for i in range(0, len(seq)):
 	if seq[i:i+3] == 'ATG': 
 		print(seq[i:i+3], i)
 
-print(seq[355:598])
-
+print(seq[126:136])
+'''
