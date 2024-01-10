c91814c580ef4f882c2f184568bdb5ae066fd725
carl <icacedo@ucdavis.edu>
2024-01-09 21:51:58 -0800

    up

--- gff_analysis/orf_checker.py
+++ gff_analysis/orf_checker.py
@@ -2,7 +2,7 @@ import sys
 
 fasta = sys.argv[1]
 wb_gff = sys.argv[2]
-apc_gff = sys.argv[3]
+apcgen_gff = sys.argv[3]
 
 with open(fasta, 'r') as fp:
 	seq = ''
@@ -34,11 +34,12 @@ with open(wb_gff, 'r') as fp:
 			count += 1
 			wbginfo['CDS'+f'-{count}'] = (int(sline[3]), int(sline[4]))
 	wbg[name] = wbginfo
+print('#####')
 
-print(wbg)
+print('*WBG*', wbg)
+wbgS = {}
 for k1 in wbg:
 	cdsnames = []
-	name = k1
 	for k2 in wbg[k1]:
 		if k2 == 'mRNA':
 			mRNA = wbg[k1][k2]
@@ -47,33 +48,35 @@ for k1 in wbg:
 	cdscrs = []
 	for cn in cdsnames:
 		cdscrs.append(wbg[k1][cn])
-	print('name: ', name)
-	print('mRNA: ', mRNA)
+	# rewrite dict w sorted CDS
+	stCDS = {}
+	print(wbgS, '******')
+	#print('name: ', k1)
+	#print('mRNA: ', mRNA)
+	stCDS['mRNA'] = mRNA
 	count = 0
 	for csc in sorted(cdscrs):
-		count += 1
-		print('CDS'+f'-{count}: ', csc)
+		count += 1 
+		stCDS['CDS'+f'-{count}'] = csc
+		#print('CDS'+f'-{count}: ', csc)
+	wbgS[k1] = stCDS
+print('*WBG SORTED*', wbgS)
+
+for gn in wbg:
+	clist = []
+	for ft in wbg[gn]:
+		if 'CDS' in ft:
+			clist.append(ft)
+	start = wbg[gn][clist[0]][0]
+	end = wbg[gn][clist[-1]][1]
+	print('wb start and stop codons', start, end)
 
 
-		
-		#print(j, wbg[i][j])
-		#beg = wbg[i][j][0] - 1
-		#end = wbg[i][j][1] - 1
-		#print(seq[beg:beg+3], seq[end-2:end+1])
-		
-
-
-
-print('##########')
-
-
-
-'''
 # goal: print ATG, # of bases, STOP	
 # stop codons: TAG, TAA, TGA
 
 aisos = {}
-with open(apc_gff, 'r') as fp:
+with open(apcgen_gff, 'r') as fp:
 	count = 0
 	for line in fp.readlines():
 		line = line.rstrip()
@@ -104,6 +107,7 @@ for aiso in aisos:
 			icount += 1
 			isoinfo['intron'+f'-{icount}'] = (int(info[3]), int(info[4]))
 	isosinfos[name] = isoinfo
+	# testing with only the top isoform
 	break
 print(isosinfos)
 
@@ -112,9 +116,10 @@ print('##########')
 # now compare sequences
 
 # apc isoforms need to use wb ATG
-for gn in wbg:
-	for ft in wbg[gn]:
-		if ft == 'CDS-1':
+for inm in isosinfos:
+	print(inm)
+
+'''
 			print(gn, ft, wbg[gn][ft])
 			start = wbg[gn][ft][0] - 1
 			print(start, seq[start:start+3])
@@ -128,6 +133,6 @@ print(seek[0:3])
 print(seek[12:15])
 print((len(seek)-6)/3)
 print(seek[3:12])
-
-# need to test an example that has only one CDS
 '''
+# need to test an example that has only one CDS
+
