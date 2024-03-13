c66fde3b17507f8eea16b8738e1fb3e8842dc84d
carl <icacedo@ucdavis.edu>
2024-03-13 13:20:38 -0700

    up

--- gff_analysis/apcgen2jb.py
+++ gff_analysis/apcgen2jb.py
@@ -1,44 +1,39 @@
 import argparse
-from grimoire.genome import Reader
-from grimoire.feature import Feature
 
 parser = argparse.ArgumentParser()
-parser.add_argument('fasta')
 parser.add_argument('gff')
+parser.add_argument('fasta')
 
 args = parser.parse_args()
-
-genome = Reader(gff=args.gff, fasta=args.fasta)
-dna = next(genome)
-
-loc, STR, gene = dna.desc.split()
-chrom, coords = loc.split(':')
-BEG, END = coords.split('-')
-BEG = int(BEG)
-END = int(END)
-
-exos = []
-ints = []
-accs = []
-dons = []
-for f in dna.ftable.features:
-	coor = (f.beg, f.end)
-	if f.type == 'intron':
-		if coor not in ints: ints.append(coor)
-		if f.beg not in dons: dons.append(f.beg)
-		if f.end not in accs: accs.append(f.end)
-	elif f.type == 'exon':
-		if coor not in exos: exos.append(coor)
 
 escores = {}
 iscores = {}
 dscores = {}
 ascores = {}
+name = None
+wbid = None
+source = None
+chrom = None
+coor = None
+strand = None
 with open(args.gff, 'r') as fp:
 	for line in fp.readlines():
 		line = line.rstrip()
 		line = line.split('\t')
+		if '# name' in line[0]:
+			name = line[0].split(' ')[2]
+		if '# wb id' in line[0]:
+			wbid = line[0].split(' ')[3]
+		if '# coordinates' in line[0]:
+			loc = line[0].split(' ')[2]
+			chrom = loc.split(':')[0]
+			coor = loc.split(':')[1]
+			coor = (int(coor.split('-')[0]), int(coor.split('-')[1]))
+		if '# strand' in line[0]:
+			strand = line[0].split(' ')[2]
 		if len(line) == 9:
+			if line[2] == 'gene':
+				source = line[1]
 			if line[2] == 'exon': 
 				exon = (int(line[3]), int(line[4]))
 				atbs = line[8].split(';')
@@ -56,37 +51,17 @@ with open(args.gff, 'r') as fp:
 					dscores[intron[0]] = dscore
 					ascores[intron[1]] = ascore
 
-gffs = []	
-for beg, end in exos:
-	gffs.append(Feature(dna, beg, end, STR, 'exon', source='model',
-				score=escores[(beg, end)]))
-for beg, end in ints:
-	s = iscores[(beg, end)]
-	gffs.append(Feature(dna, beg, end, STR, 'intron', source='model',
-				score=iscores[(beg, end)]))
-for d in dons:
-	gffs.append(Feature(dna, d, d+1, STR, 'donor', source='model',
-				score=dscores[d]))
-for a in accs:
-	gffs.append(Feature(dna, a, a+1, STR, 'acceptor', source='model',
-				score=ascores[a]))
-for f in gffs:
-	dna.ftable.add_feature(f)
+seq = ''
+with open(args.fasta, 'r') as fp:
+	for line in fp.readlines():
+		line = line.rstrip()
+		if line.startswith('>'): continue
+		seq += line
 
-if STR == '-': dna.revcomp()
-
-for f in dna.ftable.features:
-	col9 = ''
-	if f.id: col9 = f'ID={f.id}'
-	if f.pid:
-		pids = ','.join(f.pid)
-		if col9: col9 += f';Parent={pids}'
-		else: col9 = f'Parent={pids}'
-
-	print('\t'.join((chrom, f.source, f.type,
-			str(f.beg + BEG), str(f.end + END),
-			str(f.score), f.strand, '.', col9)))
-
-
-
+for intron in iscores:
+	beg = coor[0] + intron[0]
+	end = coor[0] + intron[1]
+	score = iscores[intron]
+	print(f'{chrom}\t{source}\tintron\t{beg}\t{end}\t',
+			f'{strand}\t.\tParent=Gene-{name};score={score}:name=jeans')
 
