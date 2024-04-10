65b401e5b148a4c0521137ad10f93b56236850b4
carl <icacedo@ucdavis.edu>
2024-04-09 19:12:45 -0700

    up

--- gff_analysis/2sheets.py
+++ gff_analysis/2sheets.py
@@ -4,21 +4,45 @@ import csv
 # write list of genes into csv for google sheets
 file = sys.argv[1]
 
-genes = []
+redict = {}
 with open(file, 'r') as csvfile:
 	reader = csv.DictReader(csvfile)
-	d = {}
 	for row in reader:
-		if row['wb match'] == '2':
-			d = {}
-			d['iso id'] = row['iso id']
-			d['WBGene'] = row['WBGene']
-			d['wb match'] = row['wb match']
-			genes.append(d)
+		ginfo = {}
+		ginfo['iso id'] = row['iso id']
+		ginfo['WBGene'] = row['WBGene']
+		if int(row['wb match']) not in redict:
+			redict[int(row['wb match'])] = []
+			redict[int(row['wb match'])].append(ginfo)
+		else:
+			redict[int(row['wb match'])].append(ginfo)
 
-fields = ['iso id', 'WBGene', 'wb match']
-with open('2isos.csv', 'w') as csvfile:
-	writer = csv.DictWriter(csvfile, fieldnames=fields)
-	writer.writeheader()
-	writer.writerows(genes)
-				
+redict = dict(sorted(redict.items()))
+fields = ['iso id', 'WBGene', 'wb match'] 
+for n in redict:
+	with open(f'{n}isos.csv', 'w') as csvfile:
+		genes = []
+		for g in redict[n]:
+			d = {}	
+			d['iso id'] = g['iso id'] 
+			d['WBGene'] = g['WBGene']
+			d['wb match'] = n
+			genes.append(d)		
+		writer = csv.DictWriter(csvfile, fieldnames=fields)
+		writer.writeheader()
+		writer.writerows(genes)
+		csvfile.close()
+		
+		
+
+
+
+
+
+
+
+
+
+
+
+
