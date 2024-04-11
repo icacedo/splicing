ae914817053bd43d942e5f8e2cc528020a97ba29
carl <icacedo@ucdavis.edu>
2024-04-11 16:50:58 -0700

    up

---
+++ genalg/genalgiso.py
@@ -0,0 +1,24 @@
+# notes from Network Models and Optimization pp 1-47
+# chapter: Multiobjective Genetic Algorithms
+# also https://www.geeksforgeeks.org/genetic-algorithms/#
+
+import random
+from datetime import datetime
+
+# individual
+'''
+def chromo():
+
+	individual = {
+		'genotype': {
+			'--wdpwm': random.random(),
+			'--wapwm': random.random(),
+			'
+
+
+	}
+'''
+# real number encoding best for function optimization
+# here, genotype=phenotype
+random.seed(datetime.now().timestamp())
+print(random.random())
