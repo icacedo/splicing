92cbc7c5e4256545c75bb240636f5390155e8437
carl <icacedo@ucdavis.edu>
2024-04-19 15:22:58 -0700

    up

---
+++ optiso/write_config.py
@@ -0,0 +1,67 @@
+import argparse
+import os
+import json
+
+parser = argparse.ArgumentParser()
+parser.add_argument('wb_dir', type=str, metavar='<directory>',
+	help='path to directory with wormbase fasta and gff files')
+parser.add_argument('model_dir', type=str, metavar='<directory>',
+	help='path to directory with apc model files')
+parser.add_argument('--gff', required=False, type=str, 
+	help='use wormbase gff donor/acceptor sites')
+
+parser.add_argument('--min_intron', required=False, type=int, default=35,
+	metavar='<int>', help='minimum length of intron [%(default)i]')
+parser.add_argument('--min_exon', required=False, type=int, default=25,
+	metavar='<int>', help='minimum length exon [%(default)i]')
+parser.add_argument('--max_splice', required=False, type=int, default=3,
+	metavar='<int>', help='maximum number of introns [%(default)i]')
+parser.add_argument('--flank', required=False, type=int, default=99,
+	metavar='<int>', help='genomic flank on each side [%(default)i]')
+parser.add_argument('--limit', required=False, type=int, default=20,
+	metavar='<int>', help='limit number of transcripts [%(default)i]')
+
+arg = parser.parse_args()
+
+config = {}
+
+if arg.gff: config['gff_introns'] = 'true'
+else: config['gff_introns'] = 'false'
+
+config['cli'] = {
+	'--min_exon': arg.min_exon,
+	'--min_intron': arg.min_intron,
+	'--max_splice': arg.max_splice,
+	'--flank': arg.flank,
+	'--limit': arg.limit
+}
+
+fpaths = {}
+for file in os.listdir(arg.wb_dir):
+	fsp = file.split('.')
+	name = f'{fsp[0]}.{fsp[1]}' 
+	if name not in fpaths:
+		fpaths[name] = []
+		if file.endswith('.fa'): fpaths[name].append(arg.wb_dir+file)
+		if file.endswith('.gff3'): fpaths[name].append(arg.wb_dir+file)
+	else:
+		if file.endswith('.fa'): fpaths[name].append(arg.wb_dir+file)
+		if file.endswith('.gff3'): fpaths[name].append(arg.wb_dir+file)
+
+fpaths = {name:sorted(fpaths[name]) for name in fpaths.keys()}
+
+genes = []
+count = 0
+for name in fpaths:
+	gene = {}
+	gene['name'] = name
+	gene['fasta'] = fpaths[name][0]
+	gene['gff'] = fpaths[name][1]
+	genes.append(gene)
+	count += 1
+	if count == 2: break
+
+config['data'] = genes
+
+with open('optcon.json', 'w') as jfile:
+	json.dump(config, jfile, indent=4)
