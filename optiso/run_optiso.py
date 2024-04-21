efee63fc6d2e70b073d3487e868b9a507e9e477e
ismael <icacedo@ucdavis.edu>
2024-04-20 17:52:52 -0700

    bugs

--- optiso/run_optiso.py
+++ optiso/run_optiso.py
@@ -6,21 +6,30 @@ import subprocess
 parser = argparse.ArgumentParser()
 parser.add_argument('config_dir', type=str, 
 	help='directory with config.json files for individual genes')
+parser.add_argument('--program', required=False, type=str, 
+	default='./isoformer', help='algorithm version to use %(default)s')
 parser.add_argument('--cpu', required=False, type=int, default=1, 
-	help='number of cpus to use')
+	help='number of cpus to use %(default)d')
 
 arg = parser.parse_args()
 
 sum_params = {}
 for file in os.listdir(arg.config_dir):
 	iid = file.split('.')[0]
-	name = f'ch.{iid}'
-	cmd = f'./optiso {arg.config_dir}{file} --cpu {arg.cpu}'
+	name = f'ch.{iid}'	
+	cmd = (
+		f'./optiso {arg.config_dir}{file} --program {arg.program} '
+		f'--cpu {arg.cpu}'
+	)
+	print(name)
 	result = subprocess.run(cmd, shell=True, capture_output=True)
 	jstring = result.stdout.decode('utf-8')
-	ginfo = json.loads(jstring)
-	sum_params[name] = ginfo
+	print(jstring)
+	# fails make json object, idk why
+	#ginfo = json.loads(jstring)
+	#sum_params[name] = ginfo
 
-print(sum_params)
-
+#with open('optiso_out.json', 'w') as jfile:
+#	jfile.write(json.dumps(sum_params, indent=4))
+# fails 
 	
