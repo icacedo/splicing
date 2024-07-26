26b27cf6d68619598ce82b9991ee5a18e1e085aa
carl <icacedo@ucdavis.edu>
2024-07-26 14:52:22 -0700

    up

--- random/run_isorandom.py
+++ random/run_isorandom.py
@@ -9,6 +9,7 @@ Can now execute isorandom from anywhere
 
 import argparse
 import subprocess
+import multiprocessing 
 
 parser = argparse.ArgumentParser('wrapper for isorandom.c')
 
@@ -20,6 +21,8 @@ parser.add_argument('inc', type=int, metavar='<int>',
     help='amount to increment tested sequence lengths by')
 parser.add_argument('count', type=int, metavar='<int>',
     help='number of simulations to run')
+parser.add_argument('--cpus', required=False, type=int, default=1,
+    metavar='<int>', help='number of CPUs to use [%(default)i]')
 parser.add_argument('--min_exon', required=False, type=int, default=25, 
     metavar='<int>', help='minimum exon length [%(default)i]')
 parser.add_argument('--min_intron', required=False, type=int, default=35, 
@@ -34,6 +37,10 @@ parser.add_argument('--seed', required=False, type=int, default=1,
 
 args = parser.parse_args()
 
+def worker(cmd):
+    return subprocess.run(cmd, shell=True, capture_output=True, 
+            text=True)
+'''
 with open('isorandom.out', 'w') as file:
     for i in range(args.beg_len, args.end_len + args.inc, args.inc):
         result = subprocess.run([
@@ -42,5 +49,18 @@ with open('isorandom.out', 'w') as file:
             str(args.max_splice), "--flank", str(args.flank)
         ], check=True, text=True, capture_output=True)
         file.write(result.stdout)
+'''
+
+jobs = []
+for i in range(args.beg_len, args.end_len + args.inc, args.inc):
+    cmd = (
+        f"isorandom {i} {args.count} --min_exon {args.min_exon}"
+        f"--min_intron {args.min_intron} --max_splice {args.max_splice}"
+        f"--flank {args.flank} > {i}.out"
+    )
+    jobs.append(cmd)
+
+pool = multiprocessing.Pool(args.cpus)
+pool.map(worker, jobs)
 
 
