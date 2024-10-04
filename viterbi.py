a2f418986f69ecbb72aa796d80a18778139a30d0
carl <icacedo@ucdavis.edu>
2024-10-04 16:59:28 -0700

    up

---
+++ viterbi.py
@@ -0,0 +1,6 @@
+# https://www.cis.upenn.edu/~cis2620/notes/Example-Viterbi-DNA.pdf
+# https://www.geeksforgeeks.org/viterbi-algorithm-for-hidden-markov-models-hmms/
+# biological sequence analysis: probabilistic models of proteins and nucleic acids
+# Durbin, Richard 1998
+# https://books.google.com/books?hl=en&lr=&id=HUUhAwAAQBAJ&oi=fnd&pg=PR9&dq=durbin+et+al+1998&ots=rDcZM-INkz&sig=A1m3DgHumaxcm8oO__yGZr96rF4#v=onepage&q&f=false
+# pg 6
