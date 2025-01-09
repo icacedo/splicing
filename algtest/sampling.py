1c6c36d922caa33c05935d33945e85d76da832da
carl <icacedo@ucdavis.edu>
2025-01-09 12:10:14 -0800

    up

---
+++ algtest/sampling.py
@@ -0,0 +1,57 @@
+import random
+import argparse
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument('apc_dir', type=str, help='directory with APC genes')
+
+args = parser.parse_args()
+
+'''
+introns = []
+count = 0
+for i in range(10, 40, 10):
+    int_list = [count for x in range(i)]
+    for num in int_list:
+        introns.append(num)
+    count += 1
+   
+random.shuffle(introns)
+
+sub_ints = []
+for i in range(15):
+    sub_ints.append(random.choice(introns))
+'''
+
+genes = {}
+for fname in os.listdir(args.apc_dir):
+    if fname.endswith('gff3'):
+        gid = fname.split('.')[1]
+        with open(f'{args.apc_dir}{fname}') as fp:
+            intron_counts = {}
+            for line in fp.readlines():
+                line = line.rstrip()
+                line = line.split('\t')
+                if line[1] == 'RNASeq_splice' and line[2] == 'intron':
+                    intron = (line[3], line[4])
+                    reads = line[5]
+                    intron_counts[intron] = reads
+    genes[gid] = intron_counts
+
+for gid in genes:
+    pool = {}
+    count = 0
+    for intron in genes[gid]:
+        read_count = int(float(genes[gid][intron]))
+        ilist = [count for x in range(read_count)]
+        if count == 0:
+            pool[gid] = ilist
+            count += 1
+        else:
+            pool[gid].append(ilist)
+            count += 1
+    random.shuffle(pool)
+    break
+                    
+
+
