d9cb2427d3858748334b088a5b535b3a34564ffe
carl <icacedo@ucdavis.edu>
2025-07-11 17:37:50 -0700

    up

--- algtest/smwa.py
+++ algtest/smwa.py
@@ -7,12 +7,72 @@
 seq1 = 'GAATTCAGTTA'
 seq2 = 'GGATCGA'
 
+'''
 seq1 = 'ACTGTC'
 seq2 = 'CTAG'
-
+'''
 
 
 matrix = [[0 for x in range(len(seq1)+1)] for x in range(len(seq2)+1)]
+
+
+match = 5
+mismatch = -3 
+gap = -4
+
+
+for i in range(1, len(matrix)):
+	for j in range(1, len(matrix[i])):
+
+		diag = matrix[i-1][j-1]
+		left = matrix[i][j-1]
+		up = matrix[i-1][j] 
+		
+		diag_score = 0
+		if seq1[j-1] == seq2[i-1]: 
+			diag_score += match + diag
+		else:
+			diag_score += mismatch + diag
+			
+		left_score = left + gap
+		up_score = up + gap
+		
+		score = max(diag_score, left_score, up_score)
+		if score < 0:
+			matrix[i][j] = 0
+		else:
+			matrix[i][j] = score
+		
+		
+		
+		
+		
+		
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##### print results in matrix format #####
+score_lengths = []
+for row in matrix:
+	for score in row:
+		score_lengths.append(len(str(score)))
+
+width = max(score_lengths)
 
 # dynamically change width of each cell displayed
 def add_space(string, width):
@@ -23,14 +83,6 @@ def add_space(string, width):
 		string_len += 1
 		
 	return string
-
-# print results in matrix format
-score_lengths = []
-for row in matrix:
-	for score in row:
-		score_lengths.append(len(str(score)))
-
-width = max(score_lengths)
 
 seq_string = '  ' + (' ' * (width+1))
 for nuc in seq1:
